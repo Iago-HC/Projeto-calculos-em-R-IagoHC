a222966f2d5cbd842af18b0a5fe12564b90f3cb7
Iago-HC <iago.hach@gmail.com>
2024-05-26 20:36:47 -0300

    Versão 1.01 gaussLegendre e gauss_laguerre.r
    Versão melhorada das funções.
    
    Este script em R realiza a integração numérica utilizando os métodos de quadratura de Gauss-Legendre e Gauss-Laguerre, que são técnicas para calcular integrais definidas. Para isso, ele utiliza a biblioteca pracma.

--- gaussLegendre e gauss_laguerre.r
+++ gaussLegendre e gauss_laguerre.r
@@ -1,26 +1,37 @@
-# gaussLegendre / gauss_laguerre
+# Instalação e carregamento da biblioteca necessária
 # install.packages("pracma")
 library(pracma)
 
-require(pracma)
-gaussLegendre(n = 2, a = -1, b = 1)
-
+# Funções de integração utilizando a biblioteca pracma
 gauss_legendre <- function(integrando, n_pontos, a, b, ...){
+  # Uso da função gaussLegendre da biblioteca pracma
   pontos <- gaussLegendre(n_pontos, a = a, b = b)
-  integral <- sum(pontos$w*integrando(pontos$x,...))
-  return(integral)
-}
-gauss_laguerre <- function(integrando, n.pontos, ...){
-  pontos <- gaussLaguerre(n.pontos)
-  integral <- sum(pontos$w*integrando(pontos$x,...)
-                  /exp(-pontos$x))
+  integral <- sum(pontos$w * integrando(pontos$x, ...))
   return(integral)
 }
 
+gauss_laguerre <- function(integrando, n.pontos, ...){
+  # Uso da função gaussLaguerre da biblioteca pracma
+  pontos <- gaussLaguerre(n.pontos)
+  integral <- sum(pontos$w * integrando(pontos$x, ...) / exp(-pontos$x))
+  return(integral)
+}
+
+# Função exemplo
 fx <- function(x, lambda) {
   lambda * ((exp(-(sqrt(x)))) / (sqrt(x)))
 }
-                           
-resultado = gauss_laguerre(integrando = fx, n.pontos = 20, lambda = 6)
-resultado
-round(resultado, digits = 3)
+
+# Parâmetros editáveis pelo usuário
+n_pontos <- 20  # Número de pontos para a quadratura
+lambda <- 6     # Parâmetro da função fx
+
+# Execução principal
+main <- function(){
+  resultado <- gauss_laguerre(integrando = fx, n.pontos = n_pontos, lambda = lambda)
+  resultado_arredondado <- round(resultado, digits = 3)
+  return(resultado_arredondado)
+}
+
+# Chamada da função principal
+main()
